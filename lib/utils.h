850d7da98658534015d6adef47a01c91b1d14943
o-olll <o-olll@users.noreply.github.com>
2018-10-09 15:48:22 -0700

    utils update

--- lib/utils.h
+++ lib/utils.h
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <vector>
+#include <string>
 
 namespace utils
 {
@@ -15,5 +17,37 @@ namespace utils
             std::cout << *start++ << " ";
         }
         std::cout << std::endl;
+    }
+
+    std::vector<int> strToVector(std::string s)
+    {
+        std::vector<int> res;
+        if (s.empty())
+            return res;
+
+        int i = 0, n = 0, sign = 1;
+        char c;
+
+        while (i<s.size() && (!isdigit(s[i]) && s[i]!='-'))
+            ++i;
+
+        while (i < s.size()) {
+            c = s[i];
+            if (isdigit(c)) {
+                n = n*10 + c-'0';
+            } else if (c == '-') {
+                sign = -1;
+            } else if (c == ' ' || c == '.') {
+                res.push_back(n*sign);
+                n = 0;
+                sign = 1;
+            }
+            ++i;
+        }
+
+        if (isdigit(s[i-1]))
+            res.push_back(n*sign);
+
+        return res;
     }
 }
